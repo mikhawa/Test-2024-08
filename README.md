9eb721989aec37852265381f746d49ca96b11306
mikhawa <michaeljpitz@gmail.com>
2024-09-02 12:40:54 +0200

    registration

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Form
 
+    php bin/console make:entity
+
+
+    php bin/console make:form
+
+
+Ou pour la gestion d'inscription
+
+    php bin/console make:user
+
+    composer require symfonycasts/verify-email-bundle
+
+    php bin/console make:registration-form
+
+    
+    php
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -4,15 +4,35 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use App\Form\UserType;
 
 class FrontController extends AbstractController
 {
-    #[Route('/', name: 'app_front')]
-    public function index(): Response
+    #[Route('/', name: 'app_front', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $datas = [];
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $datas = $form->getData();
+
+        }
         return $this->render('front/index.html.twig', [
             'controller_name' => 'FrontController',
+            'form' => $form,
+            'datas' => $datas
         ]);
+    }
+    #[Route('/success', name: 'task_success')]
+    public function success(): Response
+    {
+        return new Response("Success!");
     }
 }
